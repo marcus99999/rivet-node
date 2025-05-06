302dab32516d32da92a743135c40a42ff2d07f74
marcus99999 <108526943+marcus99999@users.noreply.github.com>
2025-05-06 23:38:27 +0100

    Go

--- api/run.ts
+++ api/run.ts
@@ -16,8 +16,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       return res.status(500).json({ error: 'Missing OPEN_API_KEY environment variable.' });
     }
 
-    const project = path.resolve(__dirname, 'data', 'example.rivet-project');
-    const graph = 'example-graph';
+    const project = path.resolve(__dirname, 'data', 'Mastergraph.rivet-project');
+    const graph = 'cGJILKi8TD1YSzAKUAzKV';
 
     const datasetProvider = await NodeDatasetProvider.fromProjectFile(project, {
       save: false
