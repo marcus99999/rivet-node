c0e45cd5b3f5aaaccd1001c2b3d551583502eff2
marcus99999 <108526943+marcus99999@users.noreply.github.com>
2025-06-22 10:23:45 +0100

    Go

--- api/run.ts
+++ api/run.ts
@@ -76,7 +76,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     const result = await runGraphInFile(project, {
       graph,
       remoteDebugger: undefined,
-      inputs: { input: inputs },
+      inputs: { input: inputs }
       context: {},
       externalFunctions: {},
       onUserEvent: {},
